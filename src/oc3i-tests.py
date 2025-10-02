f2fd5169ea9f783ebf565c5059fb86a313f18a89
Jeroen Minderman <Jeroen.Minderman@ons.gov.uk>
2025-10-02 13:17:05 +0100

    Built baseline test run and output

--- src/oc3i-tests.py
+++ src/oc3i-tests.py
@@ -1,13 +1,23 @@
+from pathlib import Path
+
 import pandas as pd
 from oc3i import Coder
-from oc3i.coder import get_example_file
+
+CURRENT_DIR = Path(__file__).resolve().parent
+BENCHMARK_DATA_FILE = "isco_benchmark_data.xlsx"
+
+data_file = CURRENT_DIR.parent / "data" / BENCHMARK_DATA_FILE
 
 coder = Coder(scheme="isco")
 
-dat = pd.read_csv(get_example_file())
-dat = pd.read_excel("../data/isco_benchmark_data.xlsx")
+dat = pd.read_excel(data_file, dtype={"MANUAL_ISCO": str})
+dat["TASKS"] = dat["TASKS"].fillna("")
+dat["INDUSTRY"] = dat["INDUSTRY"].fillna("")
+dat["INDUSTRY2"] = ""
+
 out = coder.code_data_frame(dat,
-                            title_column="job_title",
-                            description_column="job_description",
-                            sector_column="job_sector")
-print(out.head())
+                            title_column="TITLE",
+                            description_column="TASKS",
+                            sector_column="INDUSTRY2")
+out["MANUAL_ISCO"] = dat["MANUAL_ISCO"]
+out.to_csv("output.csv", index=False)
